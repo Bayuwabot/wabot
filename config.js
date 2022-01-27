f7297954e1aa281003e3fc5eff08c0e59e06f437
Bayuwabot <98502994+Bayuwabot@users.noreply.github.com>
2022-01-27 13:04:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ global.tum = fs.readFileSync('./src/Ah5.jpeg')
 global.tum2 = fs.readFileSync('./src/emror.jpeg')
 global.thumb = fs.readFileSync('./src/thumb.jpeg')
 global.thumb2 = fs.readFileSync('./src/emror.jpeg')
-global.owner = ['6289681378568', '6283820073017', '6283801499848','85290882526','6285240750713','6288286421519','212706611366'] // Put your number here
-global.mods = ['6285693120588'] // Want some help?
-global.prems = ['6287842147162','6285893120588','6285656615441'] // Premium user has unlimited limit
+global.owner = ['6289525518325'] // Put your number here
+global.mods = ['6289525518325'] // Want some help?
+global.prems = ['6289525518325','6281285866502'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -32,8 +32,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'I hope you\'re fine'
-global.author = 'LitRHap'
+global.packname = 'ELAINA-BOT'
+global.author = 'BAYU OFC'
 global.multiplier = 69 // The higher, The harder levelup
 
 let chalk = require('chalk')
